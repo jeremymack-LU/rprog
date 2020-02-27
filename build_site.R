4dc485f749172b243d345f071aae1f40c525dcce
jeremymack-LU <57218089+jeremymack-LU@users.noreply.github.com>
2020-02-27 08:20:07 -0500

    Test

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/jeremymack/Documents/GitHub/rprogramming/rprog"
+setwd("/Users/jeremymack/Documents/GitHub/rprogramming/rprog")
 
 #render your sweet site. 
 rmarkdown::render_site()
